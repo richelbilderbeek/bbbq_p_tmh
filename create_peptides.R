b4900c8a2cd5c967af1515bda7d6ed511cbed42b
richelbilderbeek <richel@richelbilderbeek.nl>
2020-06-15 11:45:30 +0200

    'is_on_ci' is an mhcnuggetsr function

--- create_peptides.R
+++ create_peptides.R
@@ -1,6 +1,6 @@
 # Creates peptides.csv
 
-library(bbbq)
+library(bbbqmhc)
 suppressMessages(library(dplyr))
 
 n_aas <- 9
@@ -10,7 +10,7 @@ n_aas <- 9
 # 1000: ?40 hours
 n_peptides <- 1000 # per haplotype
 
-if (is_on_ci()) {
+if (mhcnuggetsr::is_on_ci()) {
   n_peptides <- 2
 }
 
